40be09d1ab95b229848bff24156602823d84c5a0
mtanig <mtani.pub@gmail.com>
2022-03-05 15:12:02 +0900

    Allow cors.

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 from api.controllers.ForecastController import ForecastApp
 
 app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials=True
+)
 app.logger.setLevel(os.getenv('LOG_LEVEL', 'INFO'))
 app.config['JSON_AS_ASCII'] = False
 
